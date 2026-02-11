d67fe40611405e48177741b896386c9a78544689
Krishna Patil <krishnacpatil2006@gmail.com>
2026-02-11 20:08:35 +0530

    f

--- Backend/config.py
+++ Backend/config.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://echorepairs45_db_user:Lddq4RVpKlZOb5Ws@cluster0.lhd9g1g.mongodb.net/?appName=Cluster0")
 DB_NAME = os.getenv("DB_NAME", "jobrecdb")
