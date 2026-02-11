38cb491407406fc40cebb0d9c73c05b2624367b2
Krishna Patil <krishnacpatil2006@gmail.com>
2026-02-11 20:01:28 +0530

    Update Connexion.py

--- Backend/BD/Connexion.py
+++ Backend/BD/Connexion.py
@@ -3,3 +3,7 @@ from config import MONGO_URI, DB_NAME
 
 client = MongoClient(MONGO_URI)
 db = client[DB_NAME]
+# Atlas connection configuration
+MONGO_URI_ATLAS = "mongodb+srv://echorepairs45_db_user:Lddq4RVpKlZOb5Ws@cluster0.lhd9g1g.mongodb.net/?appName=Cluster0"
+client_atlas = MongoClient(MONGO_URI_ATLAS)
+db_atlas = client_atlas[DB_NAME]
